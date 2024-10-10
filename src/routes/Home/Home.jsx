6c5937e55147bc3a22fd55d68a577140e07f7e80
Bruno <brunocleao99@gmail.com>
2024-10-10 10:03:34 -0300

    Criando e configurando o cadastrar produtos

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,10 +1,30 @@
-const Home=()=>{
+import { useState, useEffect } from "react"
+
+const Home =()=>{
+
+    const [listaproduto, setListaProduto]=useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/produto")
+        .then((res)=>{
+            return res.json()
+        })
+        .then((data)=>{
+            setListaProduto(data)
+        })
+    },[])
 
     return(
         <>
-        <h1>Home</h1>
+        <div>
+            {listaproduto.map((item, index)=>(
+                <div key={index}>
+                    {item.nome}
+                   <img src={item.foto}/>
+                </div>
+            ))}            
+        </div>
         </>
     )
 }
-
 export default Home
